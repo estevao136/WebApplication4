ac70aa8f04591e5dde1359293ac6bb5b1642bf82
047091 <047091@UNI-LAB3-74>
2024-09-17 21:20:02 -0300

    Add project files.

---
+++ WebApplication4/Class/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication4.Class
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public Pessoa(string nome, string cpf, double peso, double altura)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Peso = peso;
+            Altura = altura;
+        }
+    }
+
+}
---
+++ WebApplication4/Controllers/PessoaController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication4.Class;
+
+public class PessoaController
+{
+    private List<Pessoa> pessoas = new List<Pessoa>();
+
+    
+    public void AdicionarPessoa(Pessoa pessoa)
+    {
+        pessoas.Add(pessoa);
+        Console.WriteLine("Pessoa adicionada com sucesso.");
+    }
+
+   
+    public void AtualizarPessoa(string cpf, Pessoa novaPessoa)
+    {
+        var pessoaExistente = pessoas.FirstOrDefault(p => p.Cpf == cpf);
+        if (pessoaExistente != null)
+        {
+            pessoaExistente.Nome = novaPessoa.Nome;
+            pessoaExistente.Peso = novaPessoa.Peso;
+            pessoaExistente.Altura = novaPessoa.Altura;
+            Console.WriteLine("Pessoa atualizada com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Pessoa não encontrada.");
+        }
+    }
+
+    
+    public void RemoverPessoa(string cpf)
+    {
+        var pessoa = pessoas.FirstOrDefault(p => p.Cpf == cpf);
+        if (pessoa != null)
+        {
+            pessoas.Remove(pessoa);
+            Console.WriteLine("Pessoa removida com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Pessoa não encontrada.");
+        }
+    }
+
+    
+    public void BuscarTodasPessoas()
+    {
+        if (pessoas.Count > 0)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, CPF: {pessoa.Cpf}, Peso: {pessoa.Peso}, Altura: {pessoa.Altura}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma pessoa encontrada.");
+        }
+    }
+
+    
+    public void BuscarPessoaPorCpf(string cpf)
+    {
+        var pessoa = pessoas.FirstOrDefault(p => p.Cpf == cpf);
+        if (pessoa != null)
+        {
+            Console.WriteLine($"Nome: {pessoa.Nome}, CPF: {pessoa.Cpf}, Peso: {pessoa.Peso}, Altura: {pessoa.Altura}");
+        }
+        else
+        {
+            Console.WriteLine("Pessoa não encontrada.");
+        }
+    }
+
+    
+    public void BuscarPessoasPorNome(string nome)
+    {
+        var pessoasEncontradas = pessoas.Where(p => p.Nome.Contains(nome, StringComparison.OrdinalIgnoreCase)).ToList();
+        if (pessoasEncontradas.Count > 0)
+        {
+            foreach (var pessoa in pessoasEncontradas)
+            {
+                Console.WriteLine($"Nome: {pessoa.Nome}, CPF: {pessoa.Cpf}, Peso: {pessoa.Peso}, Altura: {pessoa.Altura}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma pessoa encontrada com o nome fornecido.");
+        }
+    }
+}
